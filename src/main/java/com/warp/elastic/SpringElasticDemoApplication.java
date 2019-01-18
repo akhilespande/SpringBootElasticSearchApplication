67858800cef6d2c83c61f8bba091f3a5cc369f4a
bhaskar <bhaskarg509@gmail.com>
2019-01-18 12:40:57 +0530

    changes

--- src/main/java/com/warp/elastic/SpringElasticDemoApplication.java
+++ src/main/java/com/warp/elastic/SpringElasticDemoApplication.java
@@ -11,7 +11,6 @@ public class SpringElasticDemoApplication {
 
 	public static void main(String[] args) {
 
-		//System.out.println("hello");
 		SpringApplication.run(SpringElasticDemoApplication.class, args);
 	}
 }
