bb470d96672bf90eb1b42db3da798d6c8f09101a
akhileshpandey <akhilespande@gmail.com>
2021-03-11 10:13:21 +0530

    added login.java

---
+++ src/main/java/com/warp/elastic/model/Login.java
@@ -0,0 +1,23 @@
+package com.warp.elastic.model;
+
+public class Login {
+    String userId;
+    String pwd;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+}
